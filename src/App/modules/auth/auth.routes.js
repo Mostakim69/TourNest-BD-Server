6774c8424fab2fcda166a02a1eaa0bab4413763c
Mostakim69 <mdmostakimhosen35@gmail.com>
2025-07-28 20:44:45 +0600

    Delete old

--- src/App/modules/auth/auth.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { createJwtToken } from "./issueJwt.js";
-
-const router = Router();
-
-router.post("/create-jwt", createJwtToken);
-
-export const AuthRoutes = router;
