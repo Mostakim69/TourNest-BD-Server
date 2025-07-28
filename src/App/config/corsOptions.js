d9df868b040cc54368b6c80f5b8744b93b3c786c
Mostakim69 <mdmostakimhosen35@gmail.com>
2025-07-29 04:29:56 +0600

    Frontend Link Added

--- src/App/config/corsOptions.js
+++ src/App/config/corsOptions.js
@@ -2,7 +2,7 @@ export const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://deshguide.surge.sh",
+    "https://tour-nest-bd.web.app/",
   ],
   credentials: true,
 };
