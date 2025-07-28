7c58faf0a33bf9e991a4606758d24198890c1ad0
Mostakim69 <mdmostakimhosen35@gmail.com>
2025-07-28 20:48:41 +0600

    Error Fixed

--- src/App/routes/routes.js
+++ src/App/routes/routes.js
@@ -1,7 +1,4 @@
 // src/App/routes/routes.js
-import { AssignmentRoutes } from "../modules/assignments/assignments.route.js";
-import { AuthRoutes } from "../modules/auth/auth.routes.js";
-import { SubmissionRoutes } from "../modules/submission/submission.route.js";
 import { UserRoutes } from "../modules/users/users.route.js";
 import { PackageRoutes } from "../modules/packages/packages.route.js";
 import { BookingRoutes } from "../modules/bookings/bookings.route.js";
@@ -10,9 +7,6 @@ import { PaymentRoutes } from "../modules/payments/payments.route.js";
 import { StoryRoutes } from "../modules/stories/stories.route.js";
 
 export const routes = [
-  { path: "/auth", route: AuthRoutes },
-  { path: "/assignments", route: AssignmentRoutes },
-  { path: "/submission", route: SubmissionRoutes },
   { path: "/users", route: UserRoutes },
   { path: "/packages", route: PackageRoutes },
   { path: "/bookings", route: BookingRoutes },
