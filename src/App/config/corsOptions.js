e13d53b0a8f10d54391ba9e588a93328556df0c4
Mostakim69 <mdmostakimhosen35@gmail.com>
2025-07-29 12:54:17 +0600

    Deploy Issu Slove

--- src/App/config/corsOptions.js
+++ src/App/config/corsOptions.js
@@ -2,7 +2,7 @@ export const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://tour-nest-bd.web.app/",
+    "https://tour-nest-bd.web.app",
   ],
   credentials: true,
 };
